823916a067d0947b05c9079fd057168c22e00c44
Mihir Ojha <mihir.ojha@aarista.com>
2024-10-24 14:02:52 -0700

    adding to the previous commit of file changes

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,9 @@
-// remix.config.js
+const { createRequestHandler } = require('@remix-run/netlify');
+
 module.exports = {
+  serverBuildTarget: 'netlify',
+  devServerPort: 8002,
+  ignoredRouteFiles: ['**/.*'],
   future: {
     v2_meta: true,
     v2_errorBoundary: true,
@@ -7,7 +11,4 @@ module.exports = {
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
   },
-  serverBuildTarget: 'node-cjs',
-  devServerPort: 8002,
-  ignoredRouteFiles: ['**/.*'],
 };
